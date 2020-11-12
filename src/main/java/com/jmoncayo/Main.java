814a0763b173dc62a5f561ed099a0a3cd7f617cf
andresmonc <jaimeandresmonc@gmail.com>
2020-11-12 11:16:54 -0600

    cleaned up imports

--- src/main/java/com/jmoncayo/Main.java
+++ src/main/java/com/jmoncayo/Main.java
@@ -2,10 +2,6 @@ package com.jmoncayo;
 
 import akka.actor.typed.ActorSystem;
 import com.jmoncayo.DownDetector.PingController;
-import com.jmoncayo.ProbablyPrimeGenerator.FirstSimpleBehavior;
-import com.jmoncayo.ProbablyPrimeGenerator.ManagerBehavior;
-import com.jmoncayo.RacingSimulation.RaceControllerBehavior;
-import com.jmoncayo.RacingSimulation.RacerBehavior;
 
 public class Main {
     public static void main(String[] args) {
